d31c5c1e14a0c06a18a6a8fc325799386b4d9a76
Matthäus <webmaster@localhost>
2022-11-12 21:21:26 +0100

    add remove_head() to linked_list

--- datastructures/linked_list.py
+++ datastructures/linked_list.py
@@ -1,3 +1,7 @@
+# linked_list.py
+#
+# Implementation of a Node class and a Singly Linked List
+
 class Node:
     def __init__(self, value, next_node=None):
         self.value = value
@@ -27,15 +31,16 @@ class LinkedList:
         self.head_node = new_node
     
     def print(self):
-        string = ""
-        current_node = self.get_head_node()
+        string = "<head> "
+        head_node = self.get_head_node()
+        current_node = head_node
         while current_node:
-            if current_node.get_value() != None:
-                if string:
+            if current_node.get_value():
+                if current_node != head_node:
                     string += " -> "
                 string += str(current_node.get_value())
             current_node = current_node.get_next_node()
-        print("Linked list: ", string)
+        print("Linked list:", string, "<tail>")
     
     def remove_node(self, value_to_remove):
         current_node = self.get_head_node()
@@ -50,15 +55,27 @@ class LinkedList:
                     current_node = None
                 else:
                     current_node = next_node
+    
+    def remove_head(self):
+        current_node = self.get_head_node()
+        if current_node:
+            next_node = current_node.get_next_node()
+            if next_node:
+                self.head_node = next_node
 
     def swap_nodes(self, val1, val2):
         pass
 
 
 # Todos:
-# Remove Head Node
 # Remove all Nodes of a kind
 # Swap two nodes
+# Two Pointers (nth to last)
+# Two Pointers (Find the middle left-weighted and right-weighted)
+# Find cycle in linked list
+# Rotate list by k places
+
+
 
 
 
@@ -69,3 +86,6 @@ ll.insert_first(35)
 ll.insert_first(55)
 ll.insert_first(71)
 ll.print()
+ll.remove_head()
+ll.print()
+
