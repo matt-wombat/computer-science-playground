535d5545f36c402c738afb162b817f0904a61436
Matt <matt-wombat@mailbox.wolkevier.de>
2024-08-21 16:58:30 +0200

    add read and write file synchronously

---
+++ 10-fs-sync.js
@@ -0,0 +1,11 @@
+// Reading and writing files synchronously
+const { readFileSync, writeFileSync } = require("fs");
+
+const firstFile = readFileSync("./content/first.txt", "utf8");
+const secondFile = readFileSync("./content/second.txt", "utf8");
+
+writeFileSync(
+  "./content/result-sync.txt",
+  `Here is the result: \n${firstFile}\n${secondFile}\n`,
+  { flag: 'a' }
+);
