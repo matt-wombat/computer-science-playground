fa0b96eeab0aae90ccdb21f8fce1d4b0a9dd50a2
Matt <matt-wombat@mailbox.wolkevier.de>
2024-08-25 17:45:49 +0200

    add event-loop examples

---
+++ app.js
@@ -0,0 +1,14 @@
+/*
+How to run
+==========
+1. Uncomment desired example
+2. Open terminal and run: npm start
+*/
+
+// require('./1-event-loop-examples/1-read-file-async')
+// require('./1-event-loop-examples/2-setTimeout')
+// require('./1-event-loop-examples/3-setInterval')
+// require('./1-event-loop-examples/4-server')
+
+
+
