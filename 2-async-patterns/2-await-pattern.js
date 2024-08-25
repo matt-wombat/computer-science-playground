4896e1f110edeccf8b584295b2af1989c42e89af
Matt <matt-wombat@mailbox.wolkevier.de>
2024-08-25 18:52:09 +0200

    refactor to async-await

--- 2-async-patterns/2-await-pattern.js
+++ 2-async-patterns/2-await-pattern.js
@@ -12,6 +12,18 @@ const getText = (path) => {
   });
 };
 
-getText(__dirname + '/../content/first.txt')
-  .then((result) => console.log(result))
-  .catch((err) => console.log(err));
+const start = async () => {
+  try {
+    const first = await getText(__dirname + '/../content/first.txt');
+    const second = await getText(__dirname + '/../content/second.txt');
+    console.log(first, second);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start()
+
+// getText(__dirname + '/../content/first.txt')
+//   .then((result) => console.log(result))
+//   .catch((err) => console.log(err));
