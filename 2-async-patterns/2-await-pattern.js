3736c2232bee02df54a56fbc5a4e9fa65e65c29a
Matt <matt-wombat@mailbox.wolkevier.de>
2024-08-25 19:03:20 +0200

    refactor to implementation with promisify

--- 2-async-patterns/2-await-pattern.js
+++ 2-async-patterns/2-await-pattern.js
@@ -1,29 +1,42 @@
-const { readFile } = require('fs');
+const { readFile, writeFile } = require('fs');
+const util = require('util');
 
-const getText = (path) => {
-  return new Promise((resolve, reject) => {
-    readFile(path, 'utf8', (err, data) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(data);
-      }
-    });
-  });
-};
+// Create a functions which return promises using the built-in
+// module util. The functions will be based on the built-in functions
+// readFile() and writeFile()
+const readFilePromise = util.promisify(readFile);
+const writeFilePromise = util.promisify(writeFile);
 
 const start = async () => {
   try {
-    const first = await getText(__dirname + '/../content/first.txt');
-    const second = await getText(__dirname + '/../content/second.txt');
+    const first = await readFilePromise(__dirname + '/../content/first.txt','utf8');
+    const second = await readFilePromise(__dirname + '/../content/second.txt','utf8');
+    await writeFilePromise(
+      __dirname + '/../content/result-promisify.txt',
+      `THIS IS AWESOME: ${first} ${second}`);
+
     console.log(first, second);
   } catch (error) {
     console.log(error);
   }
 };
 
-start()
+start();
 
+// Original implementation with Promises:
+//
+// const getText = (path) => {
+//   return new Promise((resolve, reject) => {
+//     readFile(path, 'utf8', (err, data) => {
+//       if (err) {
+//         reject(err);
+//       } else {
+//         resolve(data);
+//       }
+//     });
+//   });
+// };
+//
 // getText(__dirname + '/../content/first.txt')
 //   .then((result) => console.log(result))
 //   .catch((err) => console.log(err));
