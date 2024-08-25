2dafb36bef51e17e145bb494bcbc55347593f101
Matt <matt-wombat@mailbox.wolkevier.de>
2024-08-25 19:08:20 +0200

    refactor to node.js native promises

--- 2-async-patterns/2-await-pattern.js
+++ 2-async-patterns/2-await-pattern.js
@@ -1,18 +1,25 @@
-const { readFile, writeFile } = require('fs');
-const util = require('util');
 
-// Create a functions which return promises using the built-in
-// module util. The functions will be based on the built-in functions
-// readFile() and writeFile()
-const readFilePromise = util.promisify(readFile);
-const writeFilePromise = util.promisify(writeFile);
+// // Create a functions which return promises using the built-in
+// // module util. The functions will be based on the built-in functions
+// // readFile() and writeFile()
+// const { readFile, writeFile } = require('fs');
+// const util = require('util');
+// const readFilePromise = util.promisify(readFile);
+// const writeFilePromise = util.promisify(writeFile);
+
+const { readFile, writeFile } = require('fs').promises;
 
 const start = async () => {
   try {
-    const first = await readFilePromise(__dirname + '/../content/first.txt','utf8');
-    const second = await readFilePromise(__dirname + '/../content/second.txt','utf8');
-    await writeFilePromise(
-      __dirname + '/../content/result-promisify.txt',
+    // const first = await readFilePromise(__dirname + '/../content/first.txt','utf8');
+    // const second = await readFilePromise(__dirname + '/../content/second.txt','utf8');
+    // await writeFilePromise(
+    //   __dirname + '/../content/result-promisify.txt',
+    //   `THIS IS AWESOME: ${first} ${second}`);
+    const first = await readFile(__dirname + '/../content/first.txt','utf8');
+    const second = await readFile(__dirname + '/../content/second.txt','utf8');
+    await writeFile(
+      __dirname + '/../content/result-promisify-native.txt',
       `THIS IS AWESOME: ${first} ${second}`);
 
     console.log(first, second);
