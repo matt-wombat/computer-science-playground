9790b803d251b77f087c9ed26e26215fcf8b8c65
Matt <matt-wombat@mailbox.wolkevier.de>
2024-08-27 16:02:03 +0200

    add express serving public static assets

--- 2-express-tutorial/app.js
+++ 2-express-tutorial/app.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const path = require('path');
 const app = express();
+
+// Setup static and middleware
+app.use(express.static('./public'));
 
 app.get('/', (req, res) => {
   console.log(req.method + ' ' + req.url);
-  res.status(200).send('<h1>Home Page</h1>');
+  res.sendFile(path.resolve(__dirname, './navbar-app/index.html'));
 });
 
 app.get('/about', (req, res) => {
@@ -11,12 +15,12 @@ app.get('/about', (req, res) => {
   res.status(200).send('<h1>About Page</h1>');
 });
 
-app.all('*', (req,res) => {
-  res.status(404).send('<h1>Resource not found</h1>')
-})
+app.all('*', (req, res) => {
+  res.status(404).send('<h1>Resource not found</h1>');
+});
 
 app.listen(5000, () => {
-  console.log('Server ist listening on port 5000...');
+  console.log('Server is listening on port 5000...');
 });
 
 // app.get
