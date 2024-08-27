f6dd3cffbf6c56e864621794876647dcc691b5bf
Matt <matt-wombat@mailbox.wolkevier.de>
2024-08-27 13:44:38 +0200

    add basic http server implementation

--- 2-express-tutorial/app.js
+++ 2-express-tutorial/app.js
@@ -1,1 +1,31 @@
-console.log('Express Tutorial')
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  console.log(req.method + ' ' + url);
+
+  switch (true) {
+    // home page
+    case url === '/':
+      res.writeHead(200, { 'content-type': 'text/html' });
+      res.write('<h1>Home page</h1>');
+      res.end();
+      break;
+
+    // about page
+    case url === '/about':
+      res.writeHead(200, { 'content-type': 'text/html' });
+      res.write('<h1>About page</h1>');
+      res.end();
+      break;
+
+    // 404 - not found
+    default:
+      res.writeHead(404, { 'content-type': 'text/html' });
+      res.write('<h1>Page not found</h1>');
+      res.end();
+      break;
+  }
+});
+
+server.listen(5000);
