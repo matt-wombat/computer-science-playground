9c3e99de23baf734c7ab56d3a56986a598d2da0c
Matt <matt-wombat@mailbox.wolkevier.de>
2024-08-27 14:37:19 +0200

    refactor server for serving static files

--- 2-express-tutorial/app.js
+++ 2-express-tutorial/app.js
@@ -3,6 +3,9 @@ const { readFileSync } = require('fs');
 
 // get all files
 const homePage = readFileSync('./navbar-app/index.html')
+const homeStyles = readFileSync('./navbar-app/styles.css')
+const homeImage = readFileSync('./navbar-app/logo.svg')
+const homeLogic = readFileSync('./navbar-app/browser-app.js')
 
 const server = http.createServer((req, res) => {
   const url = req.url;
@@ -20,6 +23,27 @@ const server = http.createServer((req, res) => {
     case url === '/about':
       res.writeHead(200, { 'content-type': 'text/html' });
       res.write('<h1>About page</h1>');
+      res.end();
+      break;
+
+    // styles
+    case url === '/styles.css':
+      res.writeHead(200, { 'content-type': 'text/css' });
+      res.write(homeStyles);
+      res.end();
+      break;
+
+    // logo
+    case url === '/logo.svg':
+      res.writeHead(200, { 'content-type': 'image/svg+xml' });
+      res.write(homeImage);
+      res.end();
+      break;
+
+    // logic
+    case url === '/browser-app.js':
+      res.writeHead(200, { 'content-type': 'text/javascript' });
+      res.write(homeLogic);
       res.end();
       break;
 
