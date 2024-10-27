d27304f9cb6e2c6e1399704f0ed956848a9e6c5b
Matt <matt-wombat@mailbox.wolkevier.de>
2024-10-27 14:09:48 +0100

    add swap_nodes and print to linked list exercise

--- codecademy_course_exercises/03-linked_list.py
+++ codecademy_course_exercises/03-linked_list.py
@@ -1,31 +1,30 @@
-# We'll be using our Node class
 class Node:
   def __init__(self, value, next_node=None):
     self.value = value
     self.next_node = next_node
-    
+
   def get_value(self):
     return self.value
-  
+
   def get_next_node(self):
     return self.next_node
-  
+
   def set_next_node(self, next_node):
     self.next_node = next_node
 
-# Our LinkedList class
+
 class LinkedList:
   def __init__(self, value=None):
     self.head_node = Node(value)
-  
+
   def get_head_node(self):
     return self.head_node
-  
+
   def insert_beginning(self, new_value):
     new_node = Node(new_value)
     new_node.set_next_node(self.head_node)
     self.head_node = new_node
-    
+
   def stringify_list(self):
     string_list = ""
     current_node = self.get_head_node()
@@ -34,8 +33,18 @@ class LinkedList:
         string_list += str(current_node.get_value()) + "\n"
       current_node = current_node.get_next_node()
     return string_list
-  
-  # Define your remove_node method below:
+
+  def print(self):
+    string = "<head> "
+    current_node = self.get_head_node()
+    while current_node:
+      if current_node.get_value():
+        if current_node != self.get_head_node():
+          string += " -> "
+        string += str(current_node.get_value())
+      current_node = current_node.get_next_node()
+    print("Linked list:", string, "<tail>")
+
   def remove_node(self, value_to_remove):
     current_node = self.get_head_node()
     if current_node.get_value() == value_to_remove:
@@ -49,6 +58,55 @@ class LinkedList:
         else:
           current_node = next_node
 
+  def swap_nodes(self, val1, val2):
+    print(f'Swapping {val1} with {val2}')
+
+    node1 = self.head_node
+    node2 = self.head_node
+    node1_prev = None
+    node2_prev = None
+
+    # Find val1 and val2 in linked list 
+    # and note prev-node
+    while node1 is not None:
+      if node1.get_value() == val1:
+        break
+      node1_prev = node1
+      node1 = node1.get_next_node()
+
+    while node2 is not None:
+      if node2.get_value() == val2:
+        break
+      node2_prev = node2
+      node2 = node2.get_next_node()
+
+    # Change next node of previous node to the
+    # new node, except previous node is None. 
+    # In this case change list's head_node to the new node
+    if node1_prev is None:
+      self.head_node = node2
+    else:
+      node1_prev.set_next_node(node2);
+    
+    if node2_prev is None:
+      self.head_node = node1
+    else:
+      node2_prev.set_next_node(node1);
+
+    # Change next node of both swapped nodes
+    temp = node1.get_next_node()
+    node1.set_next_node(node2.get_next_node())
+    node2.set_next_node(temp)    
+
+    # Check for edge cases
+    if (node1 is None or node2 is None):
+      print("Swap not possible - one or more element is not in the list")
+      return
+
+    if val1 == val2:
+      print("Elements are the same - no swap needed")
+      return
+
 
 ll = LinkedList()
 ll.insert_beginning(100)
@@ -57,9 +115,12 @@ ll.insert_beginning(300)
 ll.insert_beginning("Wombat1")
 ll.insert_beginning("Wombat2")
 ll.insert_beginning("Wombat3")
-print(ll.stringify_list())
+ll.print()
 
 ll.remove_node("Wombat3")
 ll.remove_node(300)
-print(ll.stringify_list())
+ll.print()
 
+ll.swap_nodes(100, 200)
+ll.swap_nodes("Wombat2", "Wombat1")
+ll.print()
