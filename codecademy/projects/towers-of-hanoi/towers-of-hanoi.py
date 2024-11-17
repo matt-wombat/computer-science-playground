83a0f9de75580ad3dbee66c65756d727ae9c894c
Matt <matt-wombat@mailbox.wolkevier.de>
2024-11-17 20:33:59 +0100

    add towers of hanoi first part

---
+++ codecademy/projects/towers-of-hanoi/towers-of-hanoi.py
@@ -0,0 +1,56 @@
+from stack import Stack
+
+print("\nLet's play Towers of Hanoi!!")
+
+#Create the Stacks
+stacks = []
+left_stack = Stack("Left")
+middle_stack = Stack("Middle")
+right_stack = Stack("Right")
+
+stacks.append(left_stack)
+stacks.append(middle_stack)
+stacks.append(right_stack)
+
+#Set up the Game
+num_disks = int(input("\nHow many disks do you want to play with?\n"))
+
+while num_disks < 3:
+  num_disks = int(input("\nEnter a number greater than or equal to 3\n"))
+
+for i in range(num_disks, 0, -1):
+  left_stack.push("Disk " + str(i))
+
+num_optimal_moves = 2 ** num_disks - 1
+
+print("\nThe fastest you can solve this game is in {0} moves.".format(num_optimal_moves))
+
+
+#Get User Input
+def get_input():
+  choices = [stack.get_name()[0] for stack in stacks]
+
+  while True:
+    for i in range(len(stacks)):
+      name = stacks[i].get_name()
+      letter = choices[i]
+      print("Enter {0} for {1}".format(letter, name))
+  
+    user_input = input('')
+
+    # if user_input in choices:
+
+    for i in range(len(stacks)):
+      if user_input == choices[i]:
+        return stacks[i]
+
+
+
+
+
+
+
+
+
+        
+#Play the Game
