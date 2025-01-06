cb618a3bddd5b31c87f702c8c5abf74411e2620d
Matt <matt-wombat@mailbox.wolkevier.de>
2025-01-06 20:05:50 +0100

    add depth-first search example implementation

--- codecademy/datastructures_algorithms_python/17-tree-traversal/bfs.py
+++ codecademy/datastructures_algorithms_python/17-tree-traversal/bfs.py
@@ -1,4 +1,5 @@
 from collections import deque
+from tree import TreeNode
 
 # Breadth-first search function
 def bfs(root_node, goal_value):
@@ -31,3 +32,23 @@ def bfs(root_node, goal_value):
 
   # return an empty path if goal not found
   return None
+
+sample_root_node = TreeNode("Home")
+docs = TreeNode("Documents")
+photos = TreeNode("Photos")
+sample_root_node.children = [docs, photos]
+my_wish = TreeNode("WishList.txt")
+my_todo = TreeNode("TodoList.txt")
+my_cat = TreeNode("Fluffy.jpg")
+my_dog = TreeNode("Spot.jpg")
+docs.children = [my_wish, my_todo]
+photos.children = [my_cat, my_dog]
+
+print(sample_root_node)
+goal_path = bfs(sample_root_node, "Fluffy.jpg")
+if goal_path is None:
+  print("No path found")
+else:
+  print("Path found")
+  for node in goal_path:
+    print(node.value)
