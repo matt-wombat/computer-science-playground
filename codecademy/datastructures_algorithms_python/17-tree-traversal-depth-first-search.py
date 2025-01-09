fe7b4161fda154295fe31b9b97a2a4b1a911f231
Matt <matt-wombat@mailbox.wolkevier.de>
2025-01-09 22:53:01 +0100

    rearrange tree traversal example implementations

--- codecademy/datastructures_algorithms_python/17-tree-traversal-depth-first-search.py
+++ codecademy/datastructures_algorithms_python/17-tree-traversal-depth-first-search.py
@@ -73,3 +73,23 @@ six = TreeNode("F")
 seven = TreeNode("G")
 two.children = [five, four]
 three.children = [seven, six]
+
+print_tree(sample_root_node)
+
+
+def dfs(root, target, path=()):
+  path = path + (root,)
+
+  if root.value == target:
+    return path
+
+  for child in root.children:
+    path_found = dfs(child, target, path)
+
+    if path_found is not None:
+      return path_found
+
+  return None
+        
+path = dfs(sample_root_node, "F")
+print(path)
