2279406efc1909bd3ce5faaaa5464193407fc579
Matt <matt-wombat@mailbox.wolkevier.de>
2025-01-19 20:33:06 +0100

    add graph search example implementation

---
+++ courses/cc/datastructures_algorithms_python/20-graph-search.py
@@ -0,0 +1,40 @@
+def dfs(graph, current_vertex, target_value, visited = None):
+  if visited is None:
+    visited = []
+  visited.append(current_vertex)
+  if current_vertex is target_value:
+    return visited
+  
+  for neighbor in graph[current_vertex]:
+    if neighbor not in visited:
+      path = dfs(graph, neighbor, target_value, visited)
+      if path:
+        return path
+      
+def bfs(graph, start_vertex, target_value):
+  path = [start_vertex]
+  vertex_and_path = [start_vertex, path]
+  bfs_queue = [vertex_and_path]
+  visited = set()
+  while bfs_queue:
+    current_vertex, path = bfs_queue.pop(0)
+    visited.add(current_vertex)
+    for neighbor in graph[current_vertex]:
+      if neighbor not in visited:
+        if neighbor is target_value:
+          return path + [neighbor]
+        else:
+          bfs_queue.append([neighbor, path + [neighbor]])
+
+subway_stations_graph = {
+    'Ostbahnhof': set(['Max-Weber-Platz', 'Marienplatz']),
+    'Marienplatz': set(['Ostbahnhof', 'Odeonsplatz', 'Karlsplatz']),
+    'Karlsplatz': set(['Marienplatz', 'Hauptbahnhof', 'Odeonsplatz']),
+    'Hauptbahnhof': set(['Karlsplatz']),
+    'Odeonsplatz': set(['Karlsplatz','Marienplatz','Max-Weber-Platz']),
+    'Max-Weber-Platz': set(['Odeonsplatz','Ostbahnhof','Arabellapark']),
+    'Arabellapark': set(['Max-Weber-Platz'])
+  }
+
+print("BFS: ", bfs(subway_stations_graph, 'Arabellapark', 'Ostbahnhof'))
+print("DFS: ", dfs(subway_stations_graph, 'Arabellapark', 'Ostbahnhof'))
