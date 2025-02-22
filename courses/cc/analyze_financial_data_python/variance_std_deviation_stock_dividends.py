2f8c33dbb1f19d409a3f727af33b90ad86df2b4a
Matt <matt-wombat@mailbox.wolkevier.de>
2025-02-22 11:19:17 +0100

    add variance and standard deviation with numpy

--- courses/cc/analyze_financial_data_python/variance_std_deviation_stock_dividends.py
+++ courses/cc/analyze_financial_data_python/variance_std_deviation_stock_dividends.py
@@ -2,5 +2,8 @@ import numpy as np
 
 dividends_microsoft_eur = [0.6358, 0.6308, 0.6364, 0.6854, 0.6876, 0.7016, 0.7016, 0.7906]
 variance_dividends_microsoft = np.var(dividends_microsoft_eur)
+stddev_dividends_microsoft = np.std(dividends_microsoft_eur)
 
-print('The variance of Microsoft stock dividends in 2023 and 2024 is', variance_dividends_microsoft)
+print('The variance of Microsoft stock dividends in 2023 and 2024 is {:.2f}%'.format(variance_dividends_microsoft * 100))
+
+print('The standard deviation of Microsoft stock dividends in 2023 and 2024 is {:.2f}%'.format(stddev_dividends_microsoft * 100))
