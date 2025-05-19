dc292ef7b2afaa04312bb6ac09a883307759b809
Matt <matt-wombat@mailbox.wolkevier.de>
2025-05-19 19:55:00 +0200

    add min-heap example implementation

---
+++ courses/cc/pass_tech_interview_python/min_heap.py
@@ -0,0 +1,121 @@
+#
+# Example implementation of Min-Heap
+#
+
+# import random number generator
+from random import randrange
+# import heap class
+
+
+class MinHeap:
+  def __init__(self):
+    self.heap_list = [None]
+    self.count = 0
+
+  def parent_idx(self, idx):
+    return idx // 2
+
+  def left_child_idx(self, idx):
+    return idx * 2
+
+  def right_child_idx(self, idx):
+    return idx * 2 + 1
+
+  def child_present(self, idx):
+    return self.left_child_idx(idx) <= self.count
+  
+  def retrieve_min(self):
+    if self.count == 0:
+      print("No items in heap")
+      return None
+    
+    min = self.heap_list[1]
+    print("Removing: {0} from {1}".format(min, self.heap_list))
+    self.heap_list[1] = self.heap_list[self.count]
+    self.count -= 1
+    self.heap_list.pop()
+    print("Last element moved to first: {0}".format(self.heap_list))        
+    self.heapify_down()
+    return min
+
+  def add(self, element):
+    self.count += 1
+    print("Adding: {0} to {1}".format(element, self.heap_list))
+    self.heap_list.append(element)
+    self.heapify_up()
+
+
+  def get_smaller_child_idx(self, idx):
+    if self.right_child_idx(idx) > self.count:
+      print("There is only a left child")
+      return self.left_child_idx(idx)
+    else:
+      left_child = self.heap_list[self.left_child_idx(idx)]
+      right_child = self.heap_list[self.right_child_idx(idx)]
+      if left_child < right_child:
+        print("Left child is smaller")
+        return self.left_child_idx(idx)
+      else:
+        print("Right child is smaller")
+        return self.right_child_idx(idx)
+    
+  def heapify_up(self):
+    idx = self.count
+    swap_count = 0
+    while self.parent_idx(idx) > 0:
+      if self.heap_list[self.parent_idx(idx)] > self.heap_list[idx]:
+        swap_count += 1
+        tmp = self.heap_list[self.parent_idx(idx)]
+        print("swapping {0} with {1}".format(tmp, self.heap_list[idx]))
+        self.heap_list[self.parent_idx(idx)] = self.heap_list[idx]
+        self.heap_list[idx] = tmp
+      idx = self.parent_idx(idx)
+
+    element_count = len(self.heap_list)
+    if element_count > 10000:
+      print("Heap of {0} elements restored with {1} swaps"
+            .format(element_count, swap_count))
+      print("")    
+      
+  def heapify_down(self):
+    idx = 1
+    # starts at 1 because we swapped first and last elements
+    swap_count = 1
+    while self.child_present(idx):
+      smaller_child_idx = self.get_smaller_child_idx(idx)
+      if self.heap_list[idx] > self.heap_list[smaller_child_idx]:
+        swap_count += 1
+        tmp = self.heap_list[smaller_child_idx]
+        self.heap_list[smaller_child_idx] = self.heap_list[idx]
+        self.heap_list[idx] = tmp
+      idx = smaller_child_idx
+
+    element_count = len(self.heap_list)
+    if element_count >= 10000:
+      print("Heap of {0} elements restored with {1} swaps"
+            .format(element_count, swap_count))
+      print("")  
+
+
+
+if __name__ == '__main__':
+  # make an instance of MinHeap
+  min_heap = MinHeap()
+
+  # populate min_heap with descending numbers
+  numbers = [n for n in range(10001, 1, -1)]
+
+  # another example numbers
+  numbers = [10, 13, 21, 61, 22, 23, 99]
+
+  print("ADDING!")
+  for el in numbers:
+    min_heap.add(el)
+
+  print("REMOVING!")
+  # remove minimum until min_heap is empty
+  min_heap.retrieve_min()
+
+  # Note: The seems not to be a complete implementation
+  # Isn't there heapifying down missing?
+
