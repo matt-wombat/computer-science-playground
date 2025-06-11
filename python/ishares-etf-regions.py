fb5e9746e4842267f62259800c5ac6c23b5a0d38
Matt <matt-wombat@mailbox.wolkevier.de>
2025-06-11 15:13:41 +0200

    refactor for looking only at stocks in iShares ETFs

--- python/ishares-etf-regions.py
+++ python/ishares-etf-regions.py
@@ -15,16 +15,22 @@ def conv_str_to_float(val):
 
 def get_msci_region(val):
   match val:
-    case 'Belgien'|'Deutschland'|'Dänemark'|'Finnland'|'Frankreich'|'Irland'|'Israel'|'Italien'|\
+    case 'Belgien'|'Deutschland'|'Dänemark'|'Finnland'|'Frankreich'|'Irland'|'Italien'|\
          'Niederlande'|'Norwegen'|'Portugal'|'Schweden'|'Schweiz'|'Spanien'|'Vereinigtes Königreich'|\
-         'Österreich'|'Griechenland'|'Kuwait'|'Polen'|'Qatar'|'Saudi-Arabien'|'Südafrika'|\
-         'Tschechien'|'Türkei'|'Ungarn'|'Ver. Arabische Emirate'|'Ägypten':
-      return "EMEA"
-    case 'Vereinigte Staaten'|'Kanada'|'Brasilien'|'Chile'|'Kolumbien'|'Mexiko'|'Peru':
-      return "Amerika"
+         'Österreich'|'Griechenland'|'Polen'|\
+         'Tschechien'|'Türkei'|'Ungarn':
+      return "Europa"
+    case 'Israel'|'Kuwait'|'Qatar'|'Saudi-Arabien'|'Ver. Arabische Emirate':
+      return "Vorderasien"
+    case 'Vereinigte Staaten'|'Kanada':
+      return "Nordamerika"
+    case 'Brasilien'|'Chile'|'Kolumbien'|'Mexiko'|'Peru':
+      return "Südamerika"
     case 'Japan'|'Australien'|'Hongkong'|'Neuseeland'|'Singapur'|'China'|'Indien'|'Indonesien'|\
          'Korea'|'Malaysia'|'Philippinen'|'Taiwan'|'Thailand':
-      return "Asia-Pazifik"
+      return "Asien-Pazifik"
+    case 'Ägypten'|'Südafrika':
+      return "Afrika"
     case _:
       return "Sonstige"
 
@@ -41,12 +47,21 @@ df = pd.read_csv(filename, skiprows = 2)
 print(df)
 print(df.describe())
 
-# Calculate fraction
-df["fraction"] = df["Gewichtung (%)"].apply(conv_str_to_float)
+market_value_etf = df.Marktwert.apply(conv_str_to_float).sum()
+market_value_stocks = df[df.Anlageklasse == "Aktien"].Marktwert.apply(conv_str_to_float).sum()
+
+print("Fondsfolumen",market_value_etf)
+print("Fondsfolumen (Nur Aktien)",market_value_stocks)
+
+# Calculate fraction for stocks only
 df["value"] = df["Marktwert"].apply(conv_str_to_float)
+
+# {:.1f}%'.format(val*100)
+df["fraction"] = df["value"] / market_value_stocks * 100
 
 # Print only rows with Anlagenklasse == Aktien
 print(df[df.Anlageklasse == "Aktien"][["Anlageklasse","Standort","Gewichtung (%)", "fraction", "Marktwert", "value"]])
+print("Summe fraction:", df[df.Anlageklasse == "Aktien"].fraction.sum())
 
 # Create new dataframe for rows with Anlagenklasse == Aktien and calculate sums per location
 df_aktien = df[df.Anlageklasse == "Aktien"].groupby(['Standort'])["fraction"].sum().reset_index()
@@ -57,3 +72,4 @@ print(df_aktien)
 
 # Calculate sums per region
 print(df_aktien.groupby(['msci_region'])["fraction"].sum().reset_index())
+print("Summe fraction:", df_aktien.fraction.sum())
